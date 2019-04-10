fa5f74c64918e5ee8c3ee7be4b5ced041c78aa88
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-04-10 13:15:05 -0400

    weds push

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron() {
   return (
     <div className="jumbotron text-center bg-dark">
-      <h1><span><a id="jumboHead" href="/">Jason's Books</a></span></h1>
+      <h1><span><a id="jumboHead">Jason's Books</a></span></h1>
         <div className="tossing">
           <a target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
           </a>
